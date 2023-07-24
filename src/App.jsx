edee3d543447b65466b7ce6cbe1619b6c51ad388
LuisEduardoRamos <luiseduardoramosg@gmail.com>
2023-07-24 03:08:02 -0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Sidebar from './components/Sidebar';
+import './styles/global.scss';
+import './styles/variables.scss';
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+}
+
+export default App;
